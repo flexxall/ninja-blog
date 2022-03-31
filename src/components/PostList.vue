37e7037236c7d9ecb0d1ec0dc412fb119ad16f23
= <=>
2022-03-31 16:13:54 -0500

    completed

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from "@vue/runtime-core"
+import SinglePost from "./SinglePost"
+export default {
+  props: ["posts"],
+  components: { SinglePost },
+  setup(props) {
+    onMounted(() => console.log("Mounted"))
+    onUnmounted(() => console.log("Unmounted"))
+    onUpdated(() => console.log("Updated"))
+  }
+}
+</script>
+
+<style></style>
